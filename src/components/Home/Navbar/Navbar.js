32018dcdb57d154409822bb6aa004b39d9e11c87
Rahman Muhammad Mahbubur <43867380+pabelmahbub@users.noreply.github.com>
2021-02-05 10:29:35 +0900

    Update Navbar.js

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -13,9 +13,7 @@ const Navbar = () => {
                     <li className="nav-item active">
                         <a className="nav-link mr-5" href="#">Home <span className="sr-only">(current)</span></a>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link mr-5" href="#">About</a>
-                    </li>
+                    
                     <li className="nav-item">
                         <a className="nav-link mr-5" href="#">歯科サービス</a>
                     </li>
